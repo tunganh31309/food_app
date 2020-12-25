524eb54d81af083fa743005d557b4890616f4789
TUNG ANH <chuthanhtunganh98@gmail.com>
2020-12-25 08:10:45 +0700

    thêm chú thích trong categories_page

--- lib/categories_page.dart
+++ lib/categories_page.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CategoriesPage extends StatelessWidget {
-   // class CategoriesPage kế thừa từ thẻ  StatelessWidget
+  // class CategoriesPage kế thừa từ thẻ  StatelessWidget
   // Vì trong này danh sách này không thêm sửa xóa
   @override
   Widget build(BuildContext context) {
     return GridView(
       children: <Widget>[],
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 300,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
+          maxCrossAxisExtent: 300, // chiều rộng max
+          childAspectRatio: 3 / 2, // tỉ lệ giữa chiều rộng chia chiều cao
+          crossAxisSpacing: 10, // độ rộng các lưới theo chiều dọc chiều ngang
           mainAxisSpacing: 10),
     );
   }
